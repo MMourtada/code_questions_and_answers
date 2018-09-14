5013edc76879bc5b16bbefa7c092252046c68c0a
MMourtada <34521557+MMourtada@users.noreply.github.com>
2018-09-14 15:24:10 -0400

    Update first_unique_char.py
    Added more comments.

--- Strings/first_unique_char.py
+++ Strings/first_unique_char.py
@@ -5,7 +5,8 @@ class Solution:
         :type s: str
         :rtype: int
         """
-      
+        #Input is a string
+        #output is the index of first unique character in the string, -1 if none
         chars = set(s)
         result = -1
         unique = [s.index(var) for var in chars if s.count(var) == 1]
